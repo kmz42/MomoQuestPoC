6a82a95aba4afaf1699603cadfecafc27dee91b6
kmz42 <kmz42@local>
2024-01-14 09:05:49 -0500

    Working on event processing

--- app/jobs/adventure_job.rb
+++ app/jobs/adventure_job.rb
@@ -12,11 +12,33 @@ class AdventureJob < ApplicationJob
     #
     # Meat of the adventure
     #
-    msg('Unimplemented, setting your character state back to idle')
+    # msg('Unimplemented, setting your character state back to idle')
+    done = false
+    while not done
+      # Sleep
+      sleep 3
+
+      # Get a random event
+      adventure_event = pick_adventure_event
+      event = adventure_event.event
+
+      # Process that event
+      logs = event.process(character)
+      for log in logs
+        msg(log)
+      end
+
+      # Update state
+      puts 'saving character'
+      character.save!
+      done = adventure_event.completes_adventure?
+      puts 'beginning next loop'
+    end
 
     #
     # Adventure outro
     #
+    msg('You return victorious from your adventure!')
 
     # Set character out of adventure TODO
     character.update!(status: Character::STATUS_IDLE)
@@ -35,5 +57,30 @@ class AdventureJob < ApplicationJob
 
   def bot
     @bot ||= MomoQuest::Bot.instance.bot
+  end
+
+  def pick_adventure_event
+    adventure_events = AdventureEvent.where(adventure: Adventure.first)
+    
+    total_weight = 0
+    for adventure_event in adventure_events do
+      total_weight = total_weight + adventure_event.weight
+    end
+
+    weight = rand(total_weight)
+
+    puts ('DEBUG: weight is' + weight.to_s + ' and total weight is ' + total_weight.to_s)
+    
+    for adventure_event in adventure_events do
+      weight = weight - adventure_event.weight
+      if weight <= 0
+        return adventure_event
+      end
+    end
+
+    # This is a bug
+    puts "Fix your dang algorithm"
+    # TODO
+    return AdventureEvent.first
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,31 @@
 class Event < ApplicationRecord
   belongs_to :enemy, optional: true
+
+  def process(character)
+    case kind
+    when 'EnemyEvent'
+      process_enemy_event(character)
+    when 'TreasureEvent'
+      process_treasure_event(character)
+    when 'FluffEvent'
+      process_fluff_event(character)
+    end
+  end
+
+  def process_enemy_event(character)
+    return ['TODO: unimplemented event with enemy: ' + enemy.name]
+  end
+
+  def process_treasure_event(character)
+    log = []
+
+    log.push('While exploring, you discover a stash of treasure!')
+    # log.push(description)
+    # log.push
+    return log
+  end
+
+  def process_fluff_event(character)
+    return ['TODO: unimplemented fluff event: ' + name]
+  end
 end
